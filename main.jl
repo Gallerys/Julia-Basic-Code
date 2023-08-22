3ac1025f4c781adef30dffc819f311c7b18f8a76
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 09:02:09 +0600

    Add files via upload

---
+++ main.jl
@@ -0,0 +1,10 @@
+# This is a comment in Julia. It's ignored by the Julia compiler and used for documentation.
+
+# Define a function that prints a greeting message
+function greet(name)
+    println("Hello, $name!")
+end
+
+# Call the greet function with different names
+greet("Alice")
+greet("Bob")
